915fbb1319d9079457c0fc52bd30c4120658c192
RandomKiddo <nghugare2@outlook.com>
2021-02-06 11:38:06 -0500

    Add Connection Block

---
+++ about.js
@@ -0,0 +1,16 @@
+function openConnect(connection, element, color){
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontent.length; i++){
+        tabcontent[i].style.display = "none";
+    }
+    
+    var tablinks = document.getElementsByClassName("tablink");
+    for (var i = 0; i < tablinks.length; i++){
+        tablinks[i].style.backgroundColor = "";
+    }
+    
+    document.getElementById(connection).style.display = "block";
+    element.style.backgroundColor = color;
+}
+
+document.getElementById("defaultOpen").click();
