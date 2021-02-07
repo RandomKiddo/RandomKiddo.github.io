741b2047f1971a1ae0b3432cff576b2d10f01c3b
RandomKiddo <nghugare2@outlook.com>
2021-02-06 22:28:13 -0500

    Update JQuery

--- about.js
+++ about.js
@@ -13,5 +13,6 @@ function openConnect(connection, element, color){
     element.style.backgroundColor = color;
 }
 $(document).ready(function(){
+    $("#defaultOpen").on('click', '#defaultOpen', openConnect('YouTube', $('#defaultOpen'), '#ff0000'));
     $("#defaultOpen").click();
 });
