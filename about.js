bc09fe6d4fe1633443409f12f3f3208405c8756f
RandomKiddo <nghugare2@outlook.com>
2021-02-06 22:34:16 -0500

    Attempt New Click

--- about.js
+++ about.js
@@ -13,7 +13,5 @@ function openConnect(connection, element, color){
     element.style.backgroundColor = color;
 }
 $(document).ready(function(){
-    $("#defaultOpen").click(function() {
-        openConnect('YouTube', this, '#ff0000');
-    });
+    $("#defaultOpen").click();
 });
