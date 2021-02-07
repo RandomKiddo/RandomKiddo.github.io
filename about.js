3a62c3c580a62f89e82a97e34c23faa398667e18
RandomKiddo <nghugare2@outlook.com>
2021-02-06 21:17:35 -0500

    Revert Loader on About

--- about.js
+++ about.js
@@ -1,10 +1,3 @@
-function buffer(){
-    var timeout = setTimeout(showPage, 3000); 
-}
-function showPage(){
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("buffer").style.display = "block";
-}
 function openConnect(connection, element, color){
     var tabcontent = document.getElementsByClassName("tabcontent");
     for (var i = 0; i < tabcontent.length; i++){
