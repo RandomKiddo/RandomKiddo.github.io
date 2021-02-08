2ac4d0b768ced0d0f5e3e56d2d9ed05ccbbe5be0
RandomKiddo <nghugare2@outlook.com>
2021-02-07 22:05:37 -0500

    Prepare Countdown JS

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ function showPage(){
     document.getElementById("loader").style.display = "none";
     document.getElementById("buffer").style.display = "block";
 }
+function countdown(date){
+    var countdownDate = new Date(date).getTime();
+    var x = setInterval(function(){
+        var now = new Date().getTime();
+        var distance = countdownDate - now;
+        var days = Math.floor(distance / (1000*60*60*24));
+        var hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60))
+        var minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+        var seconds = Math.floor((distance % (1000*60)) / 1000);
+        document.getElementById("counter").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        if (distance < 0){
+            clearInterval(x);
+            document.getElementById("counter").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+}
