8e0df375b4f5a2b581ec87888c97e1bb70e84c01
RandomKiddo <nghugare2@outlook.com>
2021-03-01 22:53:05 -0500

    Try New Urllib Request

--- index.py
+++ index.py
@@ -1,16 +1,23 @@
-from bs4 import BeautifulSoup as Soup
-import requests
+import urllib
+import json
 
-def scrape():
-    URL = 'https://www.youtube.com/channel/UCqORid7DP0chFER0SkjCb1A'
-    response = requests.get(URL)
-    soup = Soup(response.text, 'html.parser')
-    a = None
-    for _ in soup.findall('a'):
-        if _.id == 'video-title':
-            a = _
-            break
-    identifier = a.href[a.index('=')+1:]
+def find():
+    found = False
+    videos = []
+    ind = 1
+    author = 'firsttry'
+
+    inp = urllib.urlopen(r'http://gdata.youtube.com/feeds/api/videos?start-index={0}&max-results=50&alt=json&orderby=published&author={1}'.format(ind, author))
+    try:
+        response = json.load(inp)
+        inp.close()
+        rvids = response['feed']['entry']
+        for vid in rvids:
+            videos.append(vid)
+    except:
+        pass
+    
+    href = video['link'][0]['href']
+    identifier = href[href.index('=')+1:]
     return 'https://www.youtube.com/embed/{}?autoplay=1&mute=1'.format(identifier)
-
-scrape()
+        
