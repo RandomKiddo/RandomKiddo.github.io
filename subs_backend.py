897e78a1dfa6e9f110b4e25a613a8766798b5614
RandomKiddo <nghugare2@outlook.com>
2021-03-02 18:33:12 -0500

    Edit Data

--- subs_backend.py
+++ subs_backend.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup as Soup
 
-def get_subscribers(URL):
-    response = requests.get(URL='https://www.youtube.com/channel/UCqORid7DP0chFER0SkjCb1A')
+def get_subscribers(URL:str = 'https://www.youtube.com/channel/UCqORid7DP0chFER0SkjCb1A'):
+    response = requests.get(URL)
     soup = Soup(response.text, 'html.parser')
     subs = None
     for _ in soup.find_all('yt-formatted-string', {'id': 'subscriber-count'}):
